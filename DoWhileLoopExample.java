0e9c781607f18094f77878b3b18a1d27fd9f954e
AkshatGavshinde0 <128071412+AkshatGavshinde0@users.noreply.github.com>
2023-06-18 23:47:44 +0530

    loops

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,13 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int number = 1;
+        
+        do {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+            number++;
+        } while (number <= 20);
+    }
+}
+
