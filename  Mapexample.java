d20d60abc00c9ddb680c34b6ef8bbca81d6c0242
AkshatGavshinde0 <128071412+AkshatGavshinde0@users.noreply.github.com>
2023-06-26 00:00:52 +0530

    maps in java

---
+++  Mapexample.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+public class Mapexample {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("John", 25);
+        map.put("Jane", 30);
+        map.put("Alex", 28);
+
+        System.out.println(map);
+    }
+}
---
+++ Mapexample8.java
@@ -0,0 +1,12 @@
+ 
+import java.util.Map;
+
+public class Mapxample8 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        boolean isEmpty = map.isEmpty();
+        System.out.println("Map is empty: " + isEmpty);
+    }
+}
+
